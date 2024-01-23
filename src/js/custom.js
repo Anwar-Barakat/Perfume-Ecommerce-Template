deeebf5a9f628643cc93f6f43a3cf93474367edd
Anwar-Barakat <brkatanwar0@gmail.com>
2024-01-23 08:29:19 +0400

    added weekly sale section

--- src/js/custom.js
+++ src/js/custom.js
@@ -41,4 +41,41 @@ $(document).ready(function () {
   });
   $(".owl-next span").html("<i class='fas fa-chevron-right'></i>");
   $(".owl-prev span").html("<i class='fas fa-chevron-left'></i>");
+
+  // Count Down Timer
+  (function () {
+    const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+    // Set the countdown for 3 days from the current date
+    const today = new Date(),
+      countdownDate = new Date(today.getTime() + 3 * day),
+      countDown = countdownDate.getTime();
+
+    const x = setInterval(function () {
+      const now = new Date().getTime(),
+        distance = countDown - now;
+
+      document.getElementById("days").innerText = Math.floor(distance / day);
+      document.getElementById("hours").innerText = Math.floor(
+        (distance % day) / hour
+      );
+      document.getElementById("minutes").innerText = Math.floor(
+        (distance % hour) / minute
+      );
+      document.getElementById("seconds").innerText = Math.floor(
+        (distance % minute) / second
+      );
+
+      // do something later when date is reached
+      if (distance < 0) {
+        document.getElementById("headline").innerText = "It's my birthday!";
+        document.getElementById("countdown").style.display = "none";
+        document.getElementById("content").style.display = "block";
+        clearInterval(x);
+      }
+    }, 0);
+  })();
 });
