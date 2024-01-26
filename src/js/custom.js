c8b28529c83beb44f2f2d01b82daa761535b1347
Anwar-Barakat <brkatanwar0@gmail.com>
2024-01-26 09:51:26 +0400

    built the responsive for featured and products category sections

--- src/js/custom.js
+++ src/js/custom.js
@@ -32,9 +32,35 @@ $(document).ready(function () {
         items: 1,
       },
       600: {
-        items: 3,
+        items: 2,
       },
       1000: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  });
+  $("#bestSeller").owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: false,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+      1200: {
         items: 4,
       },
     },
